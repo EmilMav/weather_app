e2fdee53ae36fb2f1896f08cc3da57020acdf073
Nomad <>
2024-07-13 18:51:14 +0400

    delete learning component

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/WeatherCounter';
 import weatherReducer from '../features/WeatherSlice';
-import { apiSlice } from '../features/dog-api-slice'
-import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
-        [apiSlice.reducerPath]: apiSlice.reducer,
         weather: weatherReducer,
     },
-    middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(apiSlice.middleware);
-
-    }
 });
 
 export type AppDispatch = typeof store.dispatch;
